cb0f7a70fe3a7b8ffe9b58632766106346121985
Your Name <akif500@gmail.com>
2014-04-06 16:22:27 +0530

    Exception handling

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-Crawler
+Crawler for Bloomberg.com
 =======
+
+A simple crawler for scrapping articles from Bloomberg.com.
+
+Python Modules Dependencies: 
+
+1. BeautifulSoup 
+2. pymongo
+
+Usage:
+
+Run the Crawler.py file and make sure mongo db is running at localhost and port 27017.
--- links.py
+++ links.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 """
 A simple crawler for Bloomberg.com that crawls and stores all news articles in Mongo DB.
 
@@ -7,23 +7,39 @@ A simple crawler for Bloomberg.com that crawls and stores all news articles in M
 from bs4 import BeautifulSoup
 from urllib2 import urlopen
 from json import JSONEncoder
+import sys
 
 
-def get_links(url):
+def get_links(url): 							# It fetches the links of all articles on a specific date
 	
 	BASE_URL="http://www.bloomberg.com"
+	
+	try:
 
-	html = urlopen(url).read()
-	soup = BeautifulSoup(html, "lxml")
-	data = soup.find("ul","stories")
-	links = [BASE_URL + link.a["href"] for link in data.findAll("li")]
-	return links
+		html = urlopen(url).read()					#opens the URL and fetch the HTML
+		soup = BeautifulSoup(html, "lxml")			#opens HTML to be parsed 
+		data = soup.find("ul","stories")			#finds the tag from HTML with div class = stories
+		
+		#All the a href links are extracted and stored in a list
+		
+		links = [BASE_URL + link.a["href"] for link in data.findAll("li")]	
+		
+		return links
+
+	except:
+		print "Error fetching the news links from the website: "
+		sys.exit(1)	
 
 
 def get_articles(url):
 
-	html = urlopen(url).read()
-	soup = BeautifulSoup(html, "lxml")
+	try:
+		html = urlopen(url).read()					#opens the url and fetch HTML
+		soup = BeautifulSoup(html, "lxml")			#opens HTML to be parsed
+	except:
+		print "Error fetching the news articles from the website"
+		sys.exit(1)
+
 
 #EXTRACTING DATA FROM HTML TAGS
 
@@ -34,7 +50,7 @@ def get_articles(url):
 	
 
 #EXTRACTING THE DATA FROM META TAGS
-#SOME DATA HAVE BEEN EXTRACTED TWICE FROM META TAGS
+#SOME DATA HAVE BEEN EXTRACTED TWICE FROM META TAGS AND FROM THE HTML TAGS (JUST FOR THE SAKE OF DEMONSTRATION)
 
 
 	category = soup.findAll(attrs={"name":"category"}) 		#category of Article on the basis of which it has been sorted on the website
